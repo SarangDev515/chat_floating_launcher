99103c05437854e70fdfdd5c994e434c556d0b58
SARANG <164869908+SarangDev515@users.noreply.github.com>
2026-09-21 13:56:17 +0530

    Update Odoo 19 addon: models/res_users.py

--- models/res_users.py
+++ models/res_users.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
 from odoo import fields, models
 
 
-class Users(models.Model):
+class ResUsers(models.Model):
     _inherit = 'res.users'
 
     chat_request_enabled = fields.Boolean(
