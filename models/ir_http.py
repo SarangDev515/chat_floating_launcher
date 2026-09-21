0db9305f2630a474b36d2f1b499f02bb8d947a35
SARANG <164869908+SarangDev515@users.noreply.github.com>
2026-09-21 13:56:09 +0530

    Update Odoo 19 addon: models/ir_http.py

--- models/ir_http.py
+++ models/ir_http.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models
-from odoo.http import request
 
 
-class Http(models.AbstractModel):
+class IrHttp(models.AbstractModel):
     _inherit = 'ir.http'
 
     def session_info(self):
-        result = super(Http, self).session_info()
-        user = request.env.user
-        result['chat_request_enabled'] = bool(request.session.uid and user.chat_request_enabled)
+        result = super().session_info()
+        user = self.env.user
+        result['chat_request_enabled'] = bool(user.chat_request_enabled)
         result['chat_request_can_decide'] = bool(
-            request.session.uid and user.has_group('chat_floating_launcher.group_chat_request_accept_reject')
+            user.has_group('chat_floating_launcher_19.group_chat_request_accept_reject')
         )
         result['chat_request_can_cancel'] = bool(
-            request.session.uid and user.has_group('chat_floating_launcher.group_chat_request_cancel')
+            user.has_group('chat_floating_launcher_19.group_chat_request_cancel')
         )
         return result
